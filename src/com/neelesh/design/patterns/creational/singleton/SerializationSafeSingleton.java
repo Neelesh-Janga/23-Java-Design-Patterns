b026cbae99eb08901d73549bf057ddf8c4f13528
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-11-22 22:59:45 +0530

    Singleton v3 - Serialization-Safe Singleton class

---
+++ src/com/neelesh/design/patterns/creational/singleton/SerializationSafeSingleton.java
@@ -0,0 +1,30 @@
+package com.neelesh.design.patterns.creational.singleton;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class SerializationSafeSingleton implements Serializable {
+    private static final long UUID = 1L;
+    private static SerializationSafeSingleton singletonInstance;
+
+    private SerializationSafeSingleton(){}
+
+    public static SerializationSafeSingleton getInstance() {
+        if (singletonInstance == null) {
+            synchronized (SerializationSafeSingleton.class){
+                if(singletonInstance == null) singletonInstance = new SerializationSafeSingleton();
+            }
+        }
+        return singletonInstance;
+    }
+
+/*
+    @Serial lets us declare on those fields and methods that are part of serialization mechanism
+*/
+    @Serial
+    protected Object readResolve(){
+        return singletonInstance;
+    }
+
+    public static long getUuid() {return UUID;}
+}
--- src/com/neelesh/design/patterns/creational/singleton/SingletonTest.java
+++ src/com/neelesh/design/patterns/creational/singleton/SingletonTest.java
@@ -14,8 +14,14 @@ public class SingletonTest {
 //        EagerSingleton object1 = EagerSingleton.getInstance();
 //        EagerSingleton object2 = EagerSingleton.getInstance();
 
-        ThreadSafeSingleton object1 = ThreadSafeSingleton.getInstance();
-        ThreadSafeSingleton object2 = ThreadSafeSingleton.getInstance();
+//        ThreadSafeSingleton object1 = ThreadSafeSingleton.getInstance();
+//        ThreadSafeSingleton object2 = ThreadSafeSingleton.getInstance();
+
+//        ThreadSafeSingleton object1 = ThreadSafeSingleton.getInstance();
+//        ThreadSafeSingleton object2 = ThreadSafeSingleton.getInstance();
+
+        SerializationSafeSingleton object1 = SerializationSafeSingleton.getInstance();
+        SerializationSafeSingleton object2 = SerializationSafeSingleton.getInstance();
 
         if(object1 == object2)
             System.out.println("Both objects are same before serializing");
@@ -44,6 +50,33 @@ public class SingletonTest {
                 new ObjectInputStream(new FileInputStream("src/com/neelesh/design/patterns/creational/singleton/serialized_objects/object.obj"));
 
         ThreadSafeSingleton deserializedObject = (ThreadSafeSingleton) objectInputStream.readObject();
+
+        objectInputStream.close();
+
+        // Verification
+        if(deserializedObject.hashCode() != object1.hashCode())
+            System.out.println("Serialized and Deserialized objects are two different copies after deserializing");
+        else
+            System.out.println("Serialized and Deserialized objects are same copies after deserializing");
+
+        System.out.println("Before serializing: " + object1.hashCode());
+        System.out.println("After de-serializing: " + deserializedObject.hashCode());
+    }
+
+    private static void checkSerializationAndDeserialization(SerializationSafeSingleton object1) throws Exception{
+        // Serializing object1 to object1.obj
+        ObjectOutputStream objectOutputStream =
+                new ObjectOutputStream(new FileOutputStream("src/com/neelesh/design/patterns/creational/singleton/serialized_objects/object.obj"));
+
+        objectOutputStream.writeObject(object1);
+
+        objectOutputStream.close();
+
+        // De-Serializing object1 from object1.obj
+        ObjectInputStream objectInputStream =
+                new ObjectInputStream(new FileInputStream("src/com/neelesh/design/patterns/creational/singleton/serialized_objects/object.obj"));
+
+        SerializationSafeSingleton deserializedObject = (SerializationSafeSingleton) objectInputStream.readObject();
 
         objectInputStream.close();
 
