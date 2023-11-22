27943ee4c068a342376e9e182377151b2365bb68
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-11-22 22:40:38 +0530

    Singleton v1 - Eager way of creating beans

---
+++ src/com/neelesh/design/patterns/creational/singleton/EagerSingleton.java
@@ -0,0 +1,10 @@
+package com.neelesh.design.patterns.creational.singleton;
+
+public class EagerSingleton {
+    private static EagerSingleton singletonInstance = new EagerSingleton();
+    private EagerSingleton(){}
+
+    public static EagerSingleton getInstance() {
+        return singletonInstance;
+    }
+}
--- src/com/neelesh/design/patterns/creational/singleton/SingletonTest.java
+++ src/com/neelesh/design/patterns/creational/singleton/SingletonTest.java
@@ -1,10 +1,18 @@
 package com.neelesh.design.patterns.creational.singleton;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class SingletonTest {
 
     public static void main(String[] args) {
-        Singleton object1 = Singleton.getInstance();
-        Singleton object2 = Singleton.getInstance();
+//        Singleton object1 = Singleton.getInstance();
+//        Singleton object2 = Singleton.getInstance();
+
+        EagerSingleton object1 = EagerSingleton.getInstance();
+        EagerSingleton object2 = EagerSingleton.getInstance();
 
         if(object1 == object2)
             System.out.println("Both objects are same");
