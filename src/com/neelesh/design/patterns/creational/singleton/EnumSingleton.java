5609d47cbc92263d571d377e46e8196cbf509a08
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-12-05 13:16:33 +0530

    Added private constructor to check for eager instantiation

--- src/com/neelesh/design/patterns/creational/singleton/EnumSingleton.java
+++ src/com/neelesh/design/patterns/creational/singleton/EnumSingleton.java
@@ -3,6 +3,10 @@ package com.neelesh.design.patterns.creational.singleton;
 public enum EnumSingleton {
     ENUM_SINGLETON_INSTANCE;
 
+    private EnumSingleton() {
+        System.out.println("Enum Singleton Instance Created Successfully!");
+    }
+
     public void display() {
         System.out.println("This is how we implement singleton design that is thread-safe, serialization-safe and reflectionAPI-safe\n");
     }
