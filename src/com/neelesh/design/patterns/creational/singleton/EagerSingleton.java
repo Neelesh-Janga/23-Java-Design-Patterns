019f2989155794c0c67afa0ebe64c7456b5787fe
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-12-05 10:16:24 +0530

    Added print message to prove instance creation during class load.

--- src/com/neelesh/design/patterns/creational/singleton/EagerSingleton.java
+++ src/com/neelesh/design/patterns/creational/singleton/EagerSingleton.java
@@ -4,6 +4,7 @@ public class EagerSingleton {
     private static EagerSingleton singletonInstance = new EagerSingleton();
 
     private EagerSingleton() {
+        System.out.println("Eager Singleton Instance Created Successfully!");
     }
 
     public static EagerSingleton getInstance() {
