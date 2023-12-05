0aeee25056e02441379b4f79310c25bbbb49bdf3
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-12-05 12:33:54 +0530

    Added New Line Character

--- src/com/neelesh/design/patterns/creational/singleton/EnumSingleton.java
+++ src/com/neelesh/design/patterns/creational/singleton/EnumSingleton.java
@@ -4,6 +4,6 @@ public enum EnumSingleton {
     ENUM_SINGLETON_INSTANCE;
 
     public void display() {
-        System.out.println("This is how we implement singleton design that is thread-safe, serialization-safe and reflectionAPI-safe");
+        System.out.println("This is how we implement singleton design that is thread-safe, serialization-safe and reflectionAPI-safe\n");
     }
 }
